925589d222477db1777c8babb07b3a57714ffa61
thquan0308 <tranhoangquanthq@gmail.com>
2025-03-28 17:08:20 +0700

    readme

--- app/components/helper/scroll-to-top.jsx
+++ app/components/helper/scroll-to-top.jsx
@@ -4,13 +4,15 @@ import { useEffect, useState } from "react";
 import { FaArrowUp } from "react-icons/fa6";
 
 const DEFAULT_BTN_CLS =
-    "fixed bottom-8 right-6 z-50 flex items-center justify-center w-16 h-16 scale-115 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 p-4 hover:scale-150 transition-all duration-300 ease-out";
+    "fixed bottom-8 right-6 z-50 flex items-center justify-center w-16 h-16 scale-115 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 p-4 hover:scale-150 transition-all duration-300 ease-out hidden";
 const SCROLL_THRESHOLD = 50;
 
 const ScrollToTop = () => {
     const [btnCls, setBtnCls] = useState(DEFAULT_BTN_CLS);
 
     useEffect(() => {
+        if (typeof window === "undefined") return; // Chặn lỗi khi SSR
+
         const handleScroll = () => {
             if (window.scrollY > SCROLL_THRESHOLD) {
                 setBtnCls(DEFAULT_BTN_CLS.replace(" hidden", ""));
@@ -18,15 +20,19 @@ const ScrollToTop = () => {
                 setBtnCls(DEFAULT_BTN_CLS + " hidden");
             }
         };
+
         window.addEventListener("scroll", handleScroll, { passive: true });
+
         return () => {
-            window.removeEventListener("scroll", handleScroll, {
-                passive: true,
-            });
+            window.removeEventListener("scroll", handleScroll);
         };
     }, []);
 
-    const onClickBtn = () => window.scrollTo({ top: 0, behavior: "smooth" });
+    const onClickBtn = () => {
+        if (typeof window !== "undefined") {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        }
+    };
 
     return (
         <button className={btnCls} onClick={onClickBtn}>
