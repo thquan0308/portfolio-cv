4a2d8b4790fa69e2d4f7772293e7781162391bee
thquan0308 <tranhoangquanthq@gmail.com>
2025-03-28 17:44:50 +0700

    Convert page.js to client component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
@@ -6,26 +8,25 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { useEffect, useState } from "react";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
+  if (!res.ok) throw new Error('Failed to fetch data');
   const data = await res.json();
-
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
   return filtered;
-};
+}
 
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    getData().then((data) => setBlogs(data));
+  }, []);
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -34,5 +35,5 @@ export default async function Home() {
       <Education />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
